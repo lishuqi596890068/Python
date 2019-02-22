c190768e9860c390b46605537760bc3246989f96
59689 <596890068@qq.com>
2019-02-22 09:03:44 +0800

    考试

---
+++ day6Exam/demo2.py
@@ -0,0 +1,7 @@
+list = []
+for i in range(101,201):
+    if i%2==0:
+        continue
+    else:
+        list.append(i)
+print("素数列表",list)
---
+++ day6Exam/demo3.py
@@ -0,0 +1,7 @@
+list = [1,2,3,6,5,9]
+list2 = []
+for i in list:
+    list2.append(i)
+print(list2)
+list2.reverse()
+print(list2)
